40a86d0e5a43a1dad0243dc7131d4eb717e7b032
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2024-09-12 19:54:22 +0530

    Create a random id for groups and add the group with their id to local storage.

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ let groupName = document.getElementById('group-name');
 let groupCreateBtn = document.getElementById('group-create-btn');
 let groupList = document.getElementById('group-list');
 groupCreateBtn.addEventListener('click',function(){
-    // console.log(groupName.value);
-    groupList.innerHTML+=`<li>${groupName.value}</li>`
+    // to create a random id
+    let randomID = Math.floor(Math.random()*1000);
+    groupList.innerHTML+=`<li id=${randomID}>${groupName.value}</li>`
+    let storedGroupNames = JSON.parse(localStorage.getItem('groupNames'));
+    if (storedGroupNames == null){
+        storedGroupNames = {}
+    }
+    storedGroupNames[randomID] = groupName.value;
+    // console.log(storedGroupNames);
+    localStorage.setItem('groupNames',JSON.stringify(storedGroupNames));
+    groupName.value = '';
 });
