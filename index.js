f1cc4611b31fba4b1e80ba98995e54ec4acf3424
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2024-09-16 12:04:43 +0530

    Used trim function to remove whitespaces from group name

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const groupError = document.getElementById('group-error');
 let groupList = document.getElementById('group-list');
 groupCreateBtn.addEventListener('click',function(){
     let randomId = Date.now();
-    if (groupName.value==''){
+    if (groupName.value.trim() === ''){
         groupError.style.display="block";
     }else{
-        groupName.value = groupName.value[0].toUpperCase() + groupName.value.slice(1);
+        groupName.value = groupName.value.trim()[0].toUpperCase() + groupName.value.trim().slice(1);
         groupList.innerHTML+=`<li id=${randomId}>${groupName.value}</li>`
         groupName.value='';
         groupError.style.display = "none";
