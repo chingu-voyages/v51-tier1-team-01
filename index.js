54ce37ddb38e9db8acc89e56f467a1fa1d6b48f7
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2024-09-17 16:36:42 +0530

    Replace innerHtml and add two input fields for members

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const groupName = document.getElementById('group-name');
 const groupCreateBtn = document.getElementById('group-create-btn');
 const groupError = document.getElementById('group-error');
+const memberOne = document.getElementById('member-one');
+const memberTwo = document.getElementById('member-two');
 let groupList = document.getElementById('group-list');
+let membersList = document.getElementById('members-list');
 groupCreateBtn.addEventListener('click',function(){
     let randomId = Date.now();
-    if (groupName.value.trim() === ''){
+    if (groupName.value.trim() === ''||memberOne.value.trim()===''||memberTwo.value.trim()===''){
         groupError.style.display="block";
     }else{
         groupName.value = groupName.value.trim()[0].toUpperCase() + groupName.value.trim().slice(1);
-        groupList.innerHTML+=`<li id=${randomId}>${groupName.value}</li>`
+        memberOne.value = memberOne.value.trim()[0].toUpperCase() + memberOne.value.trim().slice(1);
+        memberTwo.value = memberTwo.value.trim()[0].toUpperCase() + memberTwo.value.trim().slice(1);
+        const newGroup = document.createElement('li');
+        newGroup.textContent = groupName.value;
+        newGroup.id = randomId;
+        groupList.appendChild(newGroup);
+        const memberOneElement = document.createElement('li');
+        memberOneElement.textContent = memberOne.value;
+        membersList.appendChild(memberOneElement);
+        const memberTwoElement = document.createElement('li');
+        memberTwoElement.textContent = memberTwo.value;
+        membersList.appendChild(memberTwoElement);
         groupName.value='';
+        memberOne.value = '';
+        memberTwo.value = '';
         groupError.style.display = "none";
     }
 });
