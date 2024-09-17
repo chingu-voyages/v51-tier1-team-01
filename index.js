3a88f51cb939b7bb32f0c3c94e0429378e4668dd
Jelena Jovicic <jejovicic@gmail.com>
2024-09-17 12:15:41 +0200

    Add friends functionality - add friend to overall friend list

--- index.js
+++ index.js
@@ -13,3 +13,60 @@ groupCreateBtn.addEventListener('click',function(){
         groupError.style.display = "none";
     }
 });
+
+// friends management
+
+const friendsList = []; // the overall friends array, the group creation button should pull friends from here
+
+function createFriend(firstName, lastName, imgSrc = './src/images/5907.jpg') { // function to create friend object from input, can be reused in group creation process
+    return { firstName, lastName, imgSrc }
+}
+
+const groupFriends = document.getElementById("list-friends"); // rendered list of all friends
+const btnAddFriend = document.getElementById('btn-add-friend'); // button to show friend creation form
+
+const formAddFriend = document.getElementById('form-add-friend'); // the friend creation form
+const inputFriendName = document.getElementById('friend-first-name');
+const inputFriendLastName = document.getElementById('friend-last-name');
+
+btnAddFriend.addEventListener("click", () => { // the add friend button just shows the form
+    formAddFriend.classList.add("form-add-visible");
+});
+
+formAddFriend.addEventListener("submit", (e) => { // function to create friend from input and add friend to overall friend array
+    e.preventDefault();
+    let firstName = inputFriendName.value;
+    let lastName = inputFriendLastName.value;
+    const friend = createFriend(firstName, lastName);
+    friendsList.push(friend);
+    console.table(friendsList);
+    inputFriendName.value = '';
+    inputFriendLastName.value = '';
+    renderFriendList();
+});
+
+function renderFriendList() {
+    groupFriends.textContent = '';
+    friendsList.forEach(friend => {
+        const listItemFriend = document.createElement("li");
+        const imgFriend = document.createElement("img");
+        imgFriend.classList.add("avatar-small");
+        imgFriend.setAttribute("src", friend.imgSrc);
+        const paraFriendName = document.createElement("p");
+        paraFriendName.textContent = `${friend.firstName} ${friend.lastName}`;
+        listItemFriend.appendChild(imgFriend);
+        listItemFriend.appendChild(paraFriendName);
+        listItemFriend.classList.add("list-item-friend");
+        groupFriends.appendChild(listItemFriend);
+        formAddFriend.classList.remove('form-add-visible');
+    })
+}
+
+
+
+
+
+
+
+
+
