a537b7d64a0f84f800a84a17c6b7f5ac01ae1a14
Jakka Balaji Mahendra <85291879+NameRectified@users.noreply.github.com>
2024-09-19 13:35:09 +0530

    Define checkEmpty function

--- src/index.js
+++ src/index.js
@@ -9,6 +9,32 @@ let memberInputs = document.getElementById('member-inputs');
 let membersArray = [];
 let allgroupObject = {};
 
+function makeElement(elementType,content){
+    const element = document.createElement(elementType);
+    element.textContent = titleCase(content);
+    return element;
+}
+
+function titleCase(word){
+    word  = word.trim()[0].toUpperCase()+word.trim().slice(1).toLowerCase();
+    return word;
+}
+
+function checkEmpty(value){
+    if (value.trim()===''){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function newMember(){
+    const newMemberInput = document.createElement('input');
+    newMemberInput.className = 'group-member';
+    newMemberInput.placeholder = "Enter member";
+    memberInputs.appendChild(newMemberInput);
+}
+
 groupCreateBtn.addEventListener('click', function () {
     let groupObject = {};
     let allMembersInput = document.querySelectorAll('.group-member');
@@ -17,12 +43,12 @@ groupCreateBtn.addEventListener('click', function () {
 
 
     allMembersInput.forEach(input => {
-        if (input.value.trim() === '') {
+        if (!checkEmpty(input.value)) {
             allFilled = false;
         }
     });
 
-    if (groupName.value.trim() === '' || allFilled === false) {
+    if (!checkEmpty(groupName.value) || allFilled === false) {
         groupError.style.display = "block";
     } else {
         groupError.style.display = "none";
@@ -30,7 +56,7 @@ groupCreateBtn.addEventListener('click', function () {
 
         membersArray = [];
         allMembersInput.forEach(input => {
-            if (input.value.trim() !== '') {
+            if (checkEmpty(input.value)) {
                 membersArray.push(titleCase(input.value));
                 input.value = '';
             }
@@ -57,20 +83,5 @@ groupCreateBtn.addEventListener('click', function () {
     console.log(allgroupObject);
 });
 
-function makeElement(elementType,content){
-    const element = document.createElement(elementType);
-    element.textContent = titleCase(content);
-    return element;
-}
 
-function titleCase(word){
-    word  = word.trim()[0].toUpperCase()+word.trim().slice(1).toLowerCase();
-    return word;
-}
-
-addAnotherMember.addEventListener('click', function () {
-    const newMemberInput = document.createElement('input');
-    newMemberInput.className = 'group-member';
-    newMemberInput.placeholder = "Enter member";
-    memberInputs.appendChild(newMemberInput);
-});
+addAnotherMember.addEventListener('click', newMember);
